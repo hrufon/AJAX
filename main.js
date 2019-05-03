cd89f063ef6cec74c6e098cd1671ff30c0923f6a
hrufon <33417744+hrufon@users.noreply.github.com>
2019-05-03 12:41:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,82 +1,83 @@
+const container = document.querySelector(".container");
 const URL = "https://test-users-api.herokuapp.com/users/";
+const button = document.querySelector("#create-user");
 
 async function getUsers() {
     try {
         const user = await axios.get(URL);
         if (user.status == 200) {
-            console.log(user.data.data)
-            renderUsers(user.data.data)
-        } else {
+            console.log(user)
+            renderUsers(user.data.data);
+        }
+        else {
             throw new Error();
         }
     }
     catch (err) {
-        console.log("Can't show users", err);
+        console.log("cannot get users", err);
     }
 }
-getUsers();
-
-const container = document.querySelector(".container");
+getUsers()
 
 function renderUsers(users) {
-    users.forEach(item => {
-        const userElement = document.createElement('div');
-        userElement.classList.add('user');
-        userElement.innerHTML = `
-        <p>Name: ${item.name}</p>
-        <p>Age: ${item.age}</p>
-        `
-        const closeButton = document.createElement('img');
-        closeButton.classList.add('close-button');
-        closeButton.src = "close.png"
-        container.append(userElement);
-        userElement.append(closeButton);
-        closeButton.addEventListener('click', () => {deleteUser(item.id, userElement)});
-  
+    users.forEach(usr => {
+        const userCard = document.createElement("div");
+        userCard.classList.add("user-card");
+        const clearBtn = document.createElement("img");
+        clearBtn.classList.add("clear-button");
+        clearBtn.src = "close.png";
+        userCard.innerHTML = `
+        <p>Name: ${usr.name} </p>  <p>Age: ${usr.age}</p> `;
+        container.append(userCard);
+        userCard.append(clearBtn);
+        clearBtn.addEventListener('click', () => { deleteUsers(usr.id, userCard) })
     })
 }
 
-async function createUsers(name, age){
-    const createUser = await axios.post(URL, {name, age})
+async function deleteUsers(userid, block) {
     try {
-        if (createUser.status == 200) {
-            console.log(createUser);
-            renderUsers([{ name, age }]);
-        }
-        else {
-            throw new Error
-        }
-    } catch (err) {
-        console.log("can't create user", err);
-    }
-}
-
-const button = document.querySelector('.add-user');
-const error = document.querySelector('.error');
-
-button.addEventListener('click', () => {
-    const name = document.querySelector(".name");
-    const age = document.querySelector(".age");;
-    if(0<age.value && age.value <100){
-        createUsers(name.value,age.value);
-        error.style.display = "none";
-    } else {
-        error.innerText = 'enter correct name and age';
-        error.style.display = "block";
-    } 
-})
-
-
-async function deleteUser(userId, box){
-    const deleteUsers = await axios.delete(URL + `${userId}`)
-    try {
-        if (deleteUsers.status == 200) {
-            box.remove();
-            console.log(deleteUsers);
+        const userDel = await axios.delete(URL + `${userid}`)
+        if (userDel.status == 200) {
+            block.remove();
+            console.log(userDel);
         }
         else throw new Error;
     }
     catch (err) {
-        console.log("can't delete user", err);
+        console.log("cannot delete users", err);
     }
 }
+
+async function createUser(name, age) {
+    const userPost = await axios.post(URL, { name, age });
+    try {
+        if (userPost.status == 200) {
+            renderUsers([{ name, age }]);
+            console.log(userPost);
+        }
+        else {
+            throw new Error
+        }
+    }
+    catch (err) {
+        console.log('cannot post', err);
+    }
+}
+
+button.addEventListener('click', () => {
+    const name = document.querySelector("#name");
+    const age = document.querySelector("#age");
+    let textMessage = "";
+
+    if (isNaN(age.value) || age.value < 0 || name.value.length === 0 || age.value.length === 0 || Number(age.value) != parseInt(age.value, 10)) {
+        textMessage = "Input must not be empty, age must be positive and integer number";
+        document.querySelector("#message").innerHTML = textMessage;
+          
+    }
+    else {
+        textMessage="";
+        document.querySelector("#message").innerHTML = textMessage;
+        createUser(name.value, age.value);
+    }
+    
+});
